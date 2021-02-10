364c49101616649dd16e8fd70d24ca80eb93181b
Vivek Kumar <mevivek3620@gmail.com>
2021-02-10 23:59:08 +0530

    Java solution

--- Tree/BSTtoGST.cpp
+++ Tree/BSTtoGST.cpp
@@ -19,3 +19,15 @@ public:
     }
 };
 
+// --->>>> Java Solution
+class Solution {
+    public int sum = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        if(root == null)return root;
+        bstToGst(root.right);
+        sum += root.val;
+        root.val = sum;
+        bstToGst(root.left);
+        return root;
+    }
+}
